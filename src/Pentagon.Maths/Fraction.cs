a3960f4a281bff158b3a51479a93a0a5fedd864f
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-11-20 14:21:53 +0100

    added fraction logic

---
+++ src/Pentagon.Maths/Fraction.cs
@@ -0,0 +1,22 @@
+﻿namespace Pentagon.Maths.SignalProcessing {
+    public abstract class Fraction<TValue>
+    {
+        public TValue Numerator { get; }
+        public TValue Denumerator { get; }
+
+        public Fraction(TValue numerator, TValue denumerator)
+        {
+            Numerator = numerator;
+            Denumerator = denumerator;
+        }
+        public static Fraction<TValue> operator +(Fraction<TValue> first, Fraction<TValue> second) => first.Add(second);
+
+        public abstract Fraction<TValue> Add(Fraction<TValue> second);
+
+        public static Fraction<TValue> operator *(Fraction<TValue> first, Fraction<TValue> second) => first.Multiple(second);
+
+        public abstract Fraction<TValue> Multiple(Fraction<TValue> second);
+
+        public abstract Fraction<TValue> InTermsOf(Fraction<TValue> second);
+    }
+}
---
+++ src/Pentagon.Maths/PolynomialFraction.cs
@@ -0,0 +1,24 @@
+﻿namespace Pentagon.Maths.SignalProcessing {
+    public class PolynomialFraction : Fraction<Polynomial>
+    {
+        public PolynomialFraction(Polynomial numerator, Polynomial denumerator) : base(numerator, denumerator) { }
+
+        public override Fraction<Polynomial> Add(Fraction<Polynomial> second)
+        {
+            var left = InTermsOf(second);
+            var right = second.InTermsOf(this);
+
+            return new PolynomialFraction(left.Numerator + right.Numerator, left.Denumerator);
+        }
+
+        public override Fraction<Polynomial> Multiple(Fraction<Polynomial> second)
+        {
+            return new PolynomialFraction(Numerator * second.Numerator, Denumerator * second.Denumerator);
+        }
+
+        public override Fraction<Polynomial> InTermsOf(Fraction<Polynomial> second)
+        {
+            return new PolynomialFraction(Numerator * second.Denumerator, Denumerator * second.Denumerator);
+        }
+    }
+}
