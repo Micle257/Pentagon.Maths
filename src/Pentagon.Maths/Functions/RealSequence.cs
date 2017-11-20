00f084af32ad3538c81b2ee3fd909bacf8740082
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-11-20 14:19:51 +0100

    sequence logic improv

--- src/Pentagon.Maths/Functions/RealSequence.cs
+++ src/Pentagon.Maths/Functions/RealSequence.cs
@@ -1,8 +1,6 @@
 namespace Pentagon.Maths.Functions {
     using System;
-    using System.Collections;
     using System.Collections.Generic;
-    using System.Linq;
     using Helpers;
     using Quantities;
 
@@ -31,51 +29,6 @@ namespace Pentagon.Maths.Functions {
             for (var i = range.Min; i < range.Max; i++)
                 ss[i] = EvaluateSample(i);
             return ss;
-        }
-    }
-
-    public class Sequence<TNumber> : IEnumerable<TNumber>
-        where TNumber : struct, IEquatable<TNumber>
-    {
-        IList<TNumber> _values;
-        int _zeroIndex;
-
-        public Sequence(IEnumerable<TNumber> values, int zeroIndex)
-        {
-            _values = values as IList<TNumber> ?? values.ToList();
-            _zeroIndex = zeroIndex;
-        }
-
-        public Sequence(IEnumerable<TNumber> zeroPositiveValues, IEnumerable<TNumber> negativeValues = null)
-        {
-            if (negativeValues != null)
-            {
-                var negatives = negativeValues as IList<TNumber> ?? negativeValues.ToList();
-                _values = negatives.Reverse().Concat(zeroPositiveValues).ToList();
-                _zeroIndex = negatives.Count;
-            }
-            else
-            {
-                _values = zeroPositiveValues as IList<TNumber> ?? zeroPositiveValues.ToList();
-                _zeroIndex = 0;
-            }
-        }
-
-        public TNumber this[int index]
-        {
-            get
-            {
-                 if (index + _zeroIndex >= _values.Count)
-                    throw new ArgumentOutOfRangeException(nameof(index));
-                return _values[index + _zeroIndex];
-            }
-        }
-
-        public IEnumerator<TNumber> GetEnumerator() => _values.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 }
