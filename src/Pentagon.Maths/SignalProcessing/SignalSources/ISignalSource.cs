3eb0dd6ef6710b9ce6fd3048195997ce7a49ae9f
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-12-22 19:50:06 +0100

    added SinSignalSource

---
+++ src/Pentagon.Maths/SignalProcessing/SignalSources/ISignalSource.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ISignalSource.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+namespace Pentagon.Maths.SignalProcessing.SignalSources
+{
+    using Quantities;
+
+    public interface ISignalSource
+    {
+        Frequency SamplingFrequency { get; }
+
+        double GetValueAt(int sample);
+    }
+}
---
+++ src/Pentagon.Maths/SignalProcessing/SignalSources/SinSignalSource.cs
@@ -0,0 +1,43 @@
+﻿namespace Pentagon.Maths.SignalProcessing.SignalSources {
+    using System;
+    using Quantities;
+    using Units;
+    using Units.Converters;
+
+    public class SinSignalSource : ISignalSource
+    {
+        public SinSignalSource(Frequency sampling, Frequency sinFrequency, double amplitude = 1 , PlaneAngle phase = default(PlaneAngle))
+        {
+            SamplingFrequency = sampling;
+            SinFrequency = sinFrequency;
+            Amplitude = amplitude;
+            Phase = phase;
+
+            _values = new double[(int)(sampling.Value/sinFrequency.Value)];
+            for (var i = 0; i < _values.Length; i++)
+            {
+                var t = i * sampling.Period;
+                _values[i] = amplitude * Math.Sin(2 * Math.PI * sinFrequency.Value * t + (phase.HasValue ? new PlaneAngleUnitConverter().ConvertUnit(phase, new Radian()).Value : 0 ));
+            }
+        }
+
+        double[] _values;
+
+        /// <inheritdoc />
+        public Frequency SamplingFrequency { get; }
+
+        public Frequency SinFrequency { get; }
+
+        public double Amplitude { get; }
+
+        public PlaneAngle Phase { get; }
+
+        /// <inheritdoc />
+        public double GetValueAt(int sample)
+        {
+            var perSample = sample % _values.Length;
+
+            return _values[perSample];
+        }
+    }
+}
